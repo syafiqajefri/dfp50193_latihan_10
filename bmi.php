459fcf044dca4476257c7eee020fd82eda97b180
syafiqajefri <syafiqajefri1705@gmail.com>
2021-10-11 13:04:31 +0800

    Update bmi.php

--- bmi.php
+++ bmi.php
@@ -4,36 +4,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BMI</title>
+    <title>B.M.I</title>
+    <style>
+        *{
+            font-family: Verdana,Geneva,Tahoma, san-serif;
+        }
+     </style>
 </head>
 <body>
-    <h1>BMI</h1>
+<h1> B.M.I </h1>
 
-    <?php
-    $berat = 60;
-    $tinggi = 1.6;
-    $bmi = 0;
-    ?>
+<?php
+$berat = 60;
+$tinggi = 1.6;
+$bmi = 0;
+?>
 
-    <table>
-        <tr>
-            <td>Berat:</td>
-            <td><?php echo $berat; ?> kilogram </td>
-        </tr>
-        <tr>
-            <td>Tinggi:</td>
-            <td><?php echo $tinggi; ?>meter</td>
-        </tr>
-        <tr>
-            <td>BMI:</td>
-            <td><?php echo $bmi; ?></td>
-        </tr>
+<table>
+    <tr>
+        <td>Berat:</td>
+        <td><?php echo $berat; ?></td><br>
+    </tr>
+
+        <td>Tinggi:</td>
+        <td><?php echo $tinggi; ?></td><br>
+    </tr>
+
+        <td>B.M.I:</td>
+        <td><?php echo number_format(bmi($berat, $tinggi),2); ?></td>
+    </tr>
     </table>
 </body>
 </html>
 
-<?php 
-function bmi($berat, $tinggi) {
 
-    return 0;
+<?php
+function bmi($berat , $tinggi) {
+    $total = $berat / $tinggi;
+    return $total;
 }
